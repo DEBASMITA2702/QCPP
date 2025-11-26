30bfda0e1c7833296f85019d6b0148ef8f3bbaa2
Debasmita Mandal <69621397+DEBASMITA2702@users.noreply.github.com>
2025-11-26 12:09:55 +0530

    Time: 0 ms (100%), Space: 8.6 MB (32.49%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,12 +1,24 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        string form = "";
-        for(char c:t){
-            if(c == s[form.size()]){
-                form.push_back(c);
+        // string form = "";
+        // for(char c:t){
+        //     if(c == s[form.size()]){
+        //         form.push_back(c);
+        //     }
+        // }
+        // return form==s;
+
+        int p1 = 0, p2 = 0;  // iterate over s and t
+        int n1 = s.size(), n2 = t.size();  // storing length of s and t
+
+        while(p1<n1 && p2<n2){
+            if(s[p1] == t[p2]){
+                p1++;
             }
+            p2++;
         }
-        return form==s;
+
+        return p1 == n1;
     }
 };
